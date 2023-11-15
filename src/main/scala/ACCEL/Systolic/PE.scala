2a14f5fed6825367df9afcf7303e1cc174a6925f
McHerman <karl@krauses.dk>
2023-11-15 16:02:10 +0100

    Changed PE multiplier to utilize verilog blackbox with DSP attribute. Synthesizes to DSP. Works in test and on FPGA

--- src/main/scala/ACCEL/Systolic/PE.scala
+++ src/main/scala/ACCEL/Systolic/PE.scala
@@ -2,9 +2,11 @@ package ATA8
 
 import chisel3._
 import chisel3.util._
+import chisel3.experimental.Analog
+import chisel3.util.HasBlackBoxInline
 
 // Multiplier Module
-class Multiplier(width: Int) extends Module {
+/* class Multiplier(width: Int) extends Module {
   val io = IO(new Bundle {
     val a = Input(UInt(width.W))
     val b = Input(UInt(width.W))
@@ -16,8 +18,29 @@ class Multiplier(width: Int) extends Module {
   when(io.enable) {
     io.result := io.a * io.b
   }
-}
+} */
 
+
+class DSPMultiplier(width: Int) extends BlackBox with HasBlackBoxInline {
+  val io = IO(new Bundle {
+    val a = Input(UInt(width.W))
+    val b = Input(UInt(width.W))
+    val result = Output(UInt(width.W)) // Output width is twice the input width
+  })
+
+  setInline("DSPMultiplier.v",
+    s"""|(* use_dsp48 = "yes" *)
+        |module DSPMultiplier #(
+        |    parameter WIDTH = $width
+        |)(
+        |    input [WIDTH-1:0] a,
+        |    input [WIDTH-1:0] b,
+        |    output [WIDTH-1:0] result
+        |);
+        |    assign result = a * b; // Perform multiplication
+        |endmodule
+        |""".stripMargin)
+}
 // PE Module
 class PE(implicit c: Configuration) extends Module {
   val io = IO(new Bundle {
@@ -27,7 +50,8 @@ class PE(implicit c: Configuration) extends Module {
     val Y_OUT = new PEY(c.arithDataWidth,1)
   })
 
-  val multiplier = Module(new Multiplier(c.arithDataWidth))
+  //val multiplier = Module(new Multiplier(c.arithDataWidth))
+  val multiplier = Module(new DSPMultiplier(c.arithDataWidth))
 
   io.Y_OUT.PEState := io.Y_IN.PEState
   io.Y_OUT.Y := DontCare
@@ -41,7 +65,7 @@ class PE(implicit c: Configuration) extends Module {
 
   multiplier.io.a := 0.U
   multiplier.io.b := 0.U
-  multiplier.io.enable := 0.U
+  //multiplier.io.enable := 0.U
 
   switch(io.Y_IN.PEState.State) {
     is(0.U) {
@@ -49,10 +73,10 @@ class PE(implicit c: Configuration) extends Module {
       multiplier.io.a := io.X_IN.X
       multiplier.io.b := YReg
       when(io.Y_IN.PEState.EN) {
-        multiplier.io.enable := true.B
+        //multiplier.io.enable := true.B
         DataReg := multiplier.io.result + io.Y_IN.Y
       }.otherwise {
-        multiplier.io.enable := false.B
+        //multiplier.io.enable := false.B
         when(io.Y_IN.PEState.Shift) {
           YReg := io.Y_IN.Y
         }
@@ -63,12 +87,12 @@ class PE(implicit c: Configuration) extends Module {
       multiplier.io.a := io.X_IN.X
       multiplier.io.b := io.Y_IN.Y
       when(io.Y_IN.PEState.EN) {
-        multiplier.io.enable := true.B
+        //multiplier.io.enable := true.B
         DataReg := multiplier.io.result + DataReg
         YReg := io.Y_IN.Y
         io.Y_OUT.Y := YReg
       }.otherwise {
-        multiplier.io.enable := false.B
+        //multiplier.io.enable := false.B
         when(io.Y_IN.PEState.Shift) {
           DataReg := io.Y_IN.Y
         }
@@ -79,57 +103,3 @@ class PE(implicit c: Configuration) extends Module {
 }
 
 
-/*
-class PE(implicit c: Configuration) extends Module {
-  val io = IO(new Bundle {
-    val X_IN = Flipped(new PEX(c.arithDataWidth))
-    val X_OUT = new PEX(c.arithDataWidth)
-    val Y_IN = Flipped(new PEY(c.arithDataWidth,1))
-    val Y_OUT = new PEY(c.arithDataWidth,1)
-  })
-
-  io.Y_OUT.PEState := io.Y_IN.PEState
-  io.Y_OUT.Y := DontCare
-
-  val DataReg = RegInit(0.U(c.arithDataWidth.W))
-  val XReg = RegInit(0.U(c.arithDataWidth.W))
-  val YReg = RegInit(0.U(c.arithDataWidth.W))
-
-  XReg := io.X_IN.X
-  io.X_OUT.X := XReg
-
-  switch(io.Y_IN.PEState.State){
-    is(0.U){
-
-      io.Y_OUT.Y := DataReg
-
-      when(io.Y_IN.PEState.EN){
-        //DataReg := ((io.X_IN.X * YReg) >> c.arithDataWidth/2) + io.Y_IN.Y
-        DataReg := ((io.X_IN.X.asUInt(16.W) * YReg)) + io.Y_IN.Y
-      }.otherwise{
-        when(io.Y_IN.PEState.Shift){
-          YReg := io.Y_IN.Y
-        }
-
-        io.Y_OUT.Y := YReg
-      }
-
-    }
-    is(1.U){
-      when(io.Y_IN.PEState.EN){
-        //DataReg := ((io.X_IN.X * io.Y_IN.Y) >> c.arithDataWidth/2) + DataReg
-        DataReg := (io.X_IN.X * io.Y_IN.Y) + DataReg
-
-        YReg := io.Y_IN.Y
-        io.Y_OUT.Y := YReg
-      }.otherwise{
-        when(io.Y_IN.PEState.Shift){
-          DataReg := io.Y_IN.Y
-        }
-
-        io.Y_OUT.Y := DataReg
-      }
-    }
-  }
-}
-*/
